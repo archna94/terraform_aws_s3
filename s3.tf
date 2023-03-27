17a1257cc8acb6d8db0311a802385f8180606e46
archana <archanavadde88@gmail.com>
2023-03-27 15:40:19 +0100

    updated_code

---
+++ s3.tf
@@ -0,0 +1,40 @@
+provider "aws" {
+  region = var.region
+}
+resource "aws_s3_bucket" "this" {
+  count = var.create_s3 ? 1 : 0
+  bucket = "${var.environment}-${var.s3_name}"
+  
+  tags = merge({ "ResourceName" = var.s3_name }, var.tags)
+}
+  resource "aws_s3_bucket_public_access_block" "this" {
+    count = var.create_block_public_access ? 1 : 0
+    bucket                  = aws_s3_bucket.this[0].id
+    block_public_acls       = var.block_public_acls
+    block_public_policy     = var.block_public_policy
+    ignore_public_acls      = var.ignore_public_acls 
+    restrict_public_buckets = var.restrict_public_buckets
+  }
+  resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this[0].bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.s3_enable_encryption ? var.s3_kms_master_key_id : null
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+  resource "aws_s3_bucket_notification" "notification" {
+    count = var.create_bucket_notification ? 1 : 0
+    bucket = aws_s3_bucket.this[0].id
+    queue {
+    queue_arn     = var.sqs_arn
+    events        = ["s3:ObjectCreated:*"]
+  }
+}
+
+  
+
+
---
+++ variables.tf
@@ -0,0 +1,72 @@
+variable "region" {
+  type = string
+  default = "eu-west-2"
+}
+variable "create_s3" {
+  type = bool
+  default = false
+}
+variable "environment" {
+  type = string
+  default = ""  
+}
+variable "s3_name"{
+  type = string
+  default = ""
+}
+variable "tags" {
+  description = "Tag map for the resource"
+  type        = map(string)
+  default     = {}
+}
+variable "create_block_public_access" {
+  type = bool
+  default = false
+}
+variable "block_public_acls" {
+  description = "Whether Amazon S3 should block public ACLs for this bucket."
+  type        = bool
+  default     = true
+}
+variable "block_public_policy" {
+  description = "Whether Amazon S3 should block public bucket policies for this bucket."
+  type        = bool
+  default     = true
+}
+variable "ignore_public_acls" {
+  description = "Whether Amazon S3 should ignore public ACLs for this bucket."
+  type        = bool
+  default     = true
+}
+variable "restrict_public_buckets" {
+  description = "Whether Amazon S3 should restrict public bucket policies for this bucket."
+  type        = bool
+  default     = true
+}
+variable "s3_enable_encryption" {
+  description = "Determines whether encryption will be created (affects all resources)"
+  type        = bool
+  default     = false
+}
+variable "s3_kms_master_key_id" {
+  description = "The ID of an AWS-managed customer master key (CMK) for s3"
+  type        = string
+  default = ""
+}
+variable "create_bucket_notification" {
+  description = "Determines whether encryption will be created (affects all resources)"
+  type        = bool
+  default     = "false"
+}
+variable "sqs_arn" {
+  description = "arn of sqs service"
+  type        = string
+  default =  ""
+}
+
+
+
+
+  
+
+  
